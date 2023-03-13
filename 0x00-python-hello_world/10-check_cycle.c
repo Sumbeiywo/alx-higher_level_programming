58484d9b1258661a4a9a394e1aac67ca0f0e3d34
Sumbeiywo <briankibett711@gmail.com>
2023-03-13 05:41:00 -0700

    Python - Hello, World

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * check-cycle- checks if a linked list contains a cycle
- * @list: linked list to check
- * return: 1 if the list has a cycle , 0 if it doesn't
+ * check_cycle - function checks if a singly linked list has a cycle in it.
+ * @list: pointer to the beginning of the node
+ * Return: 0 if no cycle, 1 if there is a cycle
  */
 
 int check_cycle(listint_t *list)
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,11 +4,10 @@
 #include<stdlib.h>
 
 /**
- * struct listint_s- sigly linked list
- * @n:integer
- * @next:points to the next code
- * Description:singly linked list node structure
- * for Holberton project
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
  */
 
 typedef struct listint_s
@@ -19,9 +18,8 @@ typedef struct listint_s
 listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t *head);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
-
